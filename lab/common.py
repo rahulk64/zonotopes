182ba256301474c18f16f57446a7247b1cc1a8f4
rahulk64 <emailrahulk64@gmail.com>
2019-12-11 01:28:22 -0600

    dont need this

--- lab/common.py
+++ lab/common.py
@@ -6,10 +6,7 @@ from math import copysign
 import numpy as np
 from numpy.linalg import norm
 
-from scipy.linalg import cho_factor, cho_solve, LinAlgError
-from scipy.sparse import issparse
 from scipy.sparse.linalg import LinearOperator, aslinearoperator
-
 
 EPS = np.finfo(float).eps
 
